a6b905a1764e7736c858a2fd8916f702db6bf91e
David Bloss <70458676+davidbloss@users.noreply.github.com>
2021-06-22 14:13:42 -0700

    upgrade test scripts to Python 3

--- ats/test/HelloATS/ats_check_log.py
+++ ats/test/HelloATS/ats_check_log.py
@@ -1,26 +1,25 @@
-#!/usr/apps/ats/7.0.1/bin/python
+#!/usr/bin/env python3
 import os
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         sys.exit(1)
 
-    logfile = sys.argv[1]
+    LOGFILE = sys.argv[1]
 
-    if os.path.exists(logfile):
-        with open(logfile, 'r') as f:
-            lines = f.readlines()
+    if os.path.exists(LOGFILE):
+        with open(LOGFILE, 'r') as f:
+            LOGFILE_CONTENTS = f.read()
 
-        for line in lines:
-            if "SUCCESS" in line:
-                print "checker success"
-                sys.exit(0)
-            elif "FAILURE" in line:
-                print "checker failed"
-                sys.exit(-1)
+        if "SUCCESS" in LOGFILE_CONTENTS:
+            print("checker success")
+            sys.exit(0)
+        elif "FAILURE" in LOGFILE_CONTENTS:
+            print("checker failed")
+            sys.exit(-1)
 
     # If we get to here, we did not find the log file
-    # or the log file did not have PASSED in it
-    print "test did not pass"
+    # or the log file did not have SUCCESS in it
+    print("test did not pass")
     sys.exit(1)
