e02ba2a776db002db731b45a41945d6f65397a41
Shawn Dawson <dawson6@llnl.gov>
2021-12-09 14:39:19 -0800

    Update version to 8.0.0

--- ats/version.py
+++ ats/version.py
@@ -1,8 +1,8 @@
 # begin of file
 #
-ATS_MAJOR = 7
-ATS_MINOR = 1
-ATS_PATCH = 1
+ATS_MAJOR = 8
+ATS_MINOR = 0
+ATS_PATCH = 0
 
 #
 # This version, constructed from the above,  is used internally by ATS
