bcc4353dd8517eb96f75200d597d2de7bc1bf2fc
Chris White <white238@llnl.gov>
2022-02-16 16:51:50 -0800

    update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ATS'
-copyright = '2011, Lawrence Livermore National Security LLC'
+copyright = '2021, Lawrence Livermore National Security LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
