41c1a95f480d6f70fbfb5d0c8d9d761c8947ef2c
Shawn Dawson <dawson6@llnl.gov>
2022-05-25 08:50:11 -0700

    Feature/dawson/kripke2 (#83)
    * Add Kripke testing
    
    * Update

---
+++ test/Kripke/ats_check_log.py
@@ -0,0 +1,33 @@
+#!/usr/apps/ats/7.0.4/bin/python
+import os
+import sys
+
+if __name__ == '__main__':
+    pwd = os.getcwd()
+
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    logfile = sys.argv[1]
+
+    if os.path.exists(logfile):
+
+        f = open( logfile, 'r')
+        lines = f.readlines()
+        f.close
+
+        for line in lines:
+            if "Differences found" in line:
+                print "test did not pass"
+                sys.exit(1)
+
+        # If we get to here, we found no differences
+        # test was good
+        print "test passed"
+        sys.exit(0)
+
+    # If we get to here, we did not find the log file
+    print "test did not pass"
+    sys.exit(1)
+
+# end of file
