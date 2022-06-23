217fc55958c582f46142108369f531f02003c186
William Hobbs <hobbs17@corona175.llnl.gov>
2022-06-23 16:59:26 -0700

    flux: created initial flux executor script
    Problem: Current ATS functionality does not include scheduling for Flux,
    the scheduler on El Capitan and other future LC machines. Expanded support
    for this will allow users to begin to port testing to current and future
    machines which use Flux as a system instance scheduler.
    
    Solution: Utilizing Flux's command line functionality, build in compatibility for
    flux in ATS. Add an entrypoint through a command-line `atsflux` which will allow
    a user to use Flux on any system that has it available in `/usr/bin/flux`.
    
    Fixes #88, flux-framework/flux-core#4256

---
+++ ats/bin/atsflux.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import subprocess as sp
+
+"""
+Wrapper to start ATS with Flux compatibility.
+
+Author: William Hobbs
+        <hobbs17@llnl.gov>
+
+"""
+
+def main():
+    """
+    Wrapper driver for running ATS tests under Flux.
+
+    Available command-line arguments:
+        --nodes= 
+          Specify a number of nodes to use, will default to use 3.
+        --bank=
+          Specify a project bank to charge, will default to use whatever your default bank is.
+        --partition=
+          Specify either the debug or batch partition, will default to use debug.
+    """
+
+    """Check to make sure a valid Flux installation is present on the system."""
+    try: 
+        version = int(sp.check_output(["flux", "-V"]).split()[1].decode("utf-8").split(".")[1])
+        if (version) < 38:
+            sys.exit(
+                f'''Error: this system does not have a current version of Flux installed. 
+                Version 0.{version}.0 is installed but >= 0.38.0 is required. Please update
+                the Flux version or use a different Flux (check which one you're using with `which flux`).
+                '''
+            )
+    except:
+        sys.exit(
+            '''Error: this system does not have a current version of Flux installed. 
+            No version of Flux was found on this system but version >= 0.38.0 required. Please update
+            the Flux version or specify a different Flux installation to use.
+            '''
+        )
+
+    sys_type = os.getenv("SYS_TYPE")
+    slurm_job_id = os.getenv("SLURM_JOB_ID")
+
+    num_nodes = 3
+    for index, arg in enumerate(sys.argv):
+        if (arg.find('nodes') >= 0):
+            (key, val) = arg.split('=',1)
+            if val.startswith('"') and val.endswith('"'):   # strip off possible quotes
+                val = val[1:-1]
+            num_nodes = str(val)
+            print("INFO: atsflux will use %s nodes" % num_nodes)
+            del sys.argv[index]
+
+    partition = "pdebug"
+    for index, arg in enumerate(sys.argv):
+        if (arg.find('partition') >= 0):
+            (key, val) = arg.split('=',1)
+            if val.startswith('"') and val.endswith('"'):   # strip off possible quotes
+                val = val[1:-1]
+            partition = str(val)
+            print("INFO: atsflux will use partition %s" % partition)
+            del sys.argv[index]
+    
+    account = "guests"
+    for index, arg in enumerate(sys.argv):
+        if (arg.find('bank') >= 0):
+            (key, val) = arg.split('=',1)
+            if val.startswith('"') and val.endswith('"'):   # strip off possible quotes
+                val = val[1:-1]
+            account = str(val)
+            print("INFO: atsflux will use bank %s" % account)
+            del sys.argv[index]
+    
+    cmd = []
+
+    if slurm_job_id == None: ## if this is on login node
+        cmd = ["salloc", f"--nodes={num_nodes}", f"--partition={partition}", f"--account={account}", "--time=60"]
+
+    """Get the path to an .ats file and find the test file."""
+    """Find the proper ATS implementation to pass the complete path."""
+    myats = os.path.join(sys.exec_prefix, 'bin', 'ats')
+
+    """The following is copied from atslite1.pyL#28-46"""
+    test_ats_found = False
+    test_ats_file = ""
+    clean_found = False
+    exclusive_found = False
+    nosub_found = False
+
+    for index, arg in enumerate(sys.argv):
+        #print arg
+        if (arg.find('=') >= 0):
+            (key, val) = arg.split('=',1)
+            sys.argv[index] = key + '="' + val + '"'
+        elif (arg.find('exclusive') >= 0):
+            exclusive_found = True
+        elif (arg.find('clean') >= 0):
+            clean_found = True
+        elif arg.endswith('.ats'):
+            test_ats_file = arg
+            if not os.path.exists(test_ats_file):
+                sys.exit("Bummer! Did not find test file %s" % (test_ats_file))
+
+    cmd.extend(["srun", f"-N{num_nodes}", f"-n{num_nodes}", "--pty", "/usr/bin/flux", "start", "-o,-S,log-filename=out"])
+    os.environ['SYS_TYPE'] = "flux00"
+    cmd.extend([myats, test_ats_file])
+    print("Executing: " + " ".join(cmd))
+
+    sp.run(cmd, text=True)
+        
+if __name__ == "__main__":
+    main()
