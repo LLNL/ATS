4dfa4f27a4ff72a5cf5e46b2793cde4432281654
Hemish Veeraboina <veeraboinahemish@gmail.com>
2023-04-19 14:14:21 -0700

    Added f strings, improved readability

--- ats/executables.py
+++ ats/executables.py
@@ -1,24 +1,22 @@
+from ats.configuration import machine
 from ats.atsut import is_valid_executable
 
-class Executable(object):
+class Executable:
     """Information about an executable to use. Can be created from string or
      list of strings."""
     def __init__ (self, value):
-        # NOTE: import done at __init__ to workaround circular dependency.
-        from ats.configuration import machine
         if isinstance(value, str):
             self.commandList = machine.split(value)
         else:
-            self.commandList = value[:]
+            self.commandList = value.copy()
         self.path = self.commandList[0]
 
     def is_valid(self):
         "Is this executable valid?"
-        if not self.path: return False
-        return is_valid_executable(self.path)
+        return bool(self.path and is_valid_executable(self.path))
 
     def __str__ (self):
         return " ".join(self.commandList)
 
     def __repr__ (self):
-        return "Executable ('%s')" % str(self)
+        return f"Executable('{str(self)}')"
