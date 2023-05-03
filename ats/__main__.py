fac1b0b9132d143451578ed57341634f6563d719
Shawn Dawson <dawson6@llnl.gov>
2023-05-03 11:43:09 -0700

    Update entry points used for scripts to return 0 on success.
    Update ats/__main__.py main function to return 0 on success
    and 1 on failure.

--- ats/__main__.py
+++ ats/__main__.py
@@ -7,8 +7,10 @@ import ats
 
 def main():
     result = ats.manager.main()
-    return result
-
+    if result:
+        return 0
+    else
+        return 1
 
 if __name__ == "__main__":
     sys.exit(main())
