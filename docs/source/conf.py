3ef4c226a40d5aeb3ef94b3cef8a5050233a462f
Chris White <white238@llnl.gov>
2024-03-13 15:02:22 -0700

    Sigh. another RTD fix. (#159)
    * remove duplicated python version
    
    * add more requirements
    
    * fix theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,15 +91,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+try:
+    import sphinx_rtd_theme
+except:
+    html_theme = 'classic'
+    html_theme_options = {
+        'codebgcolor': 'lightgrey',
+        'stickysidebar': 'true'
+    }
+    html_theme_path = []
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_options = {}
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
