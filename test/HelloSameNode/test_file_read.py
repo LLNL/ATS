bd1f9d8cade34588c900b15d62cd87d7d5b9dfeb
Shawn Dawson <dawson6@llnl.gov>
2025-03-14 15:17:44 -0700

    Update same node testing.

--- test/HelloSameNode/test_file_read.py
+++ test/HelloSameNode/test_file_read.py
@@ -2,8 +2,11 @@
 """Write a file on disk to /usr/tmp."""
 import os
 import sys
-
+import socket
 from mpi4py import MPI
+
+# Get the hostname
+hostname = socket.gethostname()
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -18,14 +21,17 @@ if __name__ == "__main__":
     TEST_FILE = "/usr/tmp/ats_test_same_node_" + node_label + ".txt"
 
     if rank == 0:
+
+        print(f"Reading a file on /usr/tmp on node %s\n" % (hostname))
+
         f = open(TEST_FILE, 'r')
         lines = f.readlines()
         for line in lines:
             print(line)
             if (line == "testing same_node option under ATS"):
-                print(f"Most Excellent! Read file correctly %s\n" % TEST_FILE)
+                print(f"Most Excellent! Read file correctly %s on node %s\n" % (TEST_FILE,hostname))
             else:
-                print(f"Bummer! Read file incorrectly %s\n" % TEST_FILE)
+                print(f"Bummer! Read file incorrectly %s on node %s\n" % (TEST_FILE,hostname))
                 os.remove(TEST_FILE)
                 sys.exit(-1)
         f.close
--- test/HelloSameNode/test_file_write.py
+++ test/HelloSameNode/test_file_write.py
@@ -2,7 +2,11 @@
 """Write a file on disk to /usr/tmp."""
 import os
 import sys
+import socket
 from mpi4py import MPI
+
+# Get the hostname
+hostname = socket.gethostname()
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -17,10 +21,13 @@ if __name__ == "__main__":
     TEST_FILE = "/usr/tmp/ats_test_same_node_" + node_label + ".txt"
 
     if (rank == 0):
+
+        print(f"Writing a file n /usr/tmp on node %s\n" % (hostname))
+
         with open(TEST_FILE, 'w') as ofp:
             ofp.write("testing same_node option under ATS")
         ofp.close()
 
-        print(f"Most Excellent! wrote file %s\n" % TEST_FILE)
+        print(f"Most Excellent! wrote file %s on node %s\n" % (TEST_FILE, hostname))
 
     sys.exit(0)
